0a0560022f4e2b828be6a26903fc1fd6e9cf8209
Joey Bright <hello@joeybright.com>
2015-10-27 19:00:12 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# gulp-static-starter
+
+A boilerplate I use to create static sites with Gulp.
+
+## To get started
+
+Make sure npm and bower are installed, then install dependencies for npm:
+
+      npm install
+
+And bower:
+
+      bower install
+
+Then, run gulp
+
+      gulp
+
+This will watch for any changes in the /src directory and build out the site to /build
---
+++ gulpfile.js
@@ -0,0 +1,79 @@
+
+var gulp = require('gulp'),
+    hb   = require('gulp-hb'), // gulp-handlebars plugin
+    sass = require('gulp-sass'),
+    concatinate = require('gulp-concat'),
+    watch = require('gulp-watch'),
+    minifyCss = require('gulp-minify-css'),
+    htmlmin = require('gulp-htmlmin'),
+    uncss = require('gulp-uncss'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename');
+
+// Handlebars compiling of .hbs and .json data files
+gulp.task('handlebars', function () {
+    return gulp.src('./src/pages/*.hbs')
+        .pipe(hb({
+            data: './src/data/*.json',
+            helpers: './node_modules/handlebars-layouts/index.js',
+            partials: './src/assets/partials/**/*.hbs',
+            bustCache: true
+        }))
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(rename(function (path) {
+          path.extname = ".html";
+          return path;
+        }))
+        .pipe(gulp.dest('./build'));
+});
+
+// CSS task
+gulp.task('styles', ['handlebars'], function () {
+  gulp.src('./src/assets/styles/app.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(uncss({
+        html: ['./build/**/*.html']
+    }))
+    .pipe(minifyCss({compatibility: 'ie9'}))
+    .pipe(gulp.dest('./build'));
+});
+
+// JS task
+gulp.task('scripts', function() {
+  return gulp.src('./src/assets/scripts/*.js')
+    .pipe(concatinate('app.js'))
+    .pipe(gulp.dest('./build'));
+});
+
+// Images task
+// Currently just places images located in the src/assets/images directory
+// into the /build directory. Need to add minification later.
+gulp.task('images', function() {
+  gulp.src(['./src/assets/images/*.png',
+            './src/assets/images/*.svg'])
+    .pipe(imagemin({
+            progressive: true
+        }))
+    .pipe(gulp.dest('./build'));
+});
+
+// Default (watch) task
+gulp.task('watch', function() {
+  // Watch .hbs/.html files
+  gulp.watch([
+    './src/assets/partials/**/*.hbs',
+    './src/*.hbs',
+    './src/data/*.json'],
+  ['handlebars']);
+  // Watch .scss files
+  gulp.watch('./src/assets/styles/**/**/*.scss', ['styles']);
+  // Watch .js files
+  gulp.watch('./src/assets/scripts/**/*.js', ['scripts']);
+  // Watch images
+  gulp.watch([
+    './src/assets/images/*.png',
+    './src/assets/images/*.svg'
+  ], ['images']);
+});
+
+gulp.task('default', ['handlebars', 'styles', 'scripts', 'images', 'watch']);
